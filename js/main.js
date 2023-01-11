dc7cd84ae2891b10402c481fed97f2aba7cc4374
jadins24 <jadinesh04@gmail.com>
2023-01-11 16:32:04 +0530

    add

---
+++ js/main.js
@@ -0,0 +1,68 @@
+let x=10,y=20;
+function Expression(a,b) {
+    return c=a+b;
+}
+console.log(Expression( x,y=x+y));
+
+
+function Expression(a,b) {
+    return c=a+b;
+}
+console.log(Expression( x,y=x+y*2));
+
+
+function Expression(a,b) {
+    return c=a+b;
+}
+console.log(Expression( x,y=x%10+y));
+
+
+function Expression(a,b) {
+    return c=a+b;
+}
+console.log(Expression( x,y=x));
+
+
+function Expression(a,b) {
+    return c=a+b;
+}
+console.log(Expression( x,y=x+y/2));
+
+
+function Expression(a,b) {
+    return c=a+b;
+}
+console.log(Expression( x,y=x*2+y%2));
+
+function Expression(a,b) {
+    return c=a+b;
+}
+console.log(Expression( x,y=x+y%2));
+
+function Expression(a,b) {
+    return c=a+b;
+}
+console.log(Expression( x,y=x*2+y*10));
+
+function Expression(a,b) {
+    return c=a+b;
+}
+console.log(Expression( x,y=x%5+y/2));
+
+function Expression(a,b) {
+    return c=a+b;
+}
+console.log(Expression( x,y=x+y+12));
+
+function Expression(a,b) {
+    return c=a+b;
+}
+console.log(Expression( x,y=x*y));
+
+function Expression(a,b) {
+    return c=a+b;
+}
+console.log(Expression( x,y=x-y));
+
+
+
